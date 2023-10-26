2e1f3503eb2d9753d0e7934cc8e725ba1725dd71
ZuberoClase <azuberogondam1b@gmail.com>
2023-10-26 18:35:21 +0200

    ventana controles

---
+++ ejercicio_1/La primera Main Window en WPF de Aitor Zubero/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace La_primera_Main_Window_en_WPF_de_Aitor_Zubero
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
