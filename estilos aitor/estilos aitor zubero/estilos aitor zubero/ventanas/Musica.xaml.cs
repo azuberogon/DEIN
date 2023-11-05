89245e53043cad4fd273da468ced6435b31e49a5
azuberogon <azuberogondam1b@gmail.com>
2023-11-05 18:35:01 +0100

    Ventanas 1,2,3 termiandas
    falta el codigo de c# para poder moverse entre las ventanas

---
+++ estilos aitor/estilos aitor zubero/estilos aitor zubero/ventanas/Musica.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace estilos_aitor_zubero.ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para Musica.xaml
+    /// </summary>
+    public partial class Musica : Window
+    {
+        public Musica()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
