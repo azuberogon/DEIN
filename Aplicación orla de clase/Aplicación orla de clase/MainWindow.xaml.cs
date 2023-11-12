c28893a967597f4a857cb68c48fd7a6912dea721
azuberogon <azuberogondam1b@gmail.com>
2023-11-13 00:11:01 +0100

    Fallos

---
+++ Aplicación orla de clase/Aplicación orla de clase/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplicación_orla_de_clase
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Persona41_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Profile win2 = new Profile("Nombre", "Apellidos",
+            "email_alumno@educacion.navarra.es", "/Imagenes/PersonajeGenerico.jpg");
+            win2.Show();
+        }
+    }
+}
