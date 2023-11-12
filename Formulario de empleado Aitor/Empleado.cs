515d34faa99b9300ce6ca03dc5a17b798bcfa42e
azuberogon <azuberogondam1b@gmail.com>
2023-11-12 13:56:29 +0100

    fallo en el codigo cs

---
+++ Formulario de empleado Aitor/Empleado.cs
@@ -0,0 +1,10 @@
+﻿namespace Formulario_de_empleado_Aitor
+{
+    internal class Empleado
+    {
+        public string Nombre { get; internal set; }
+        public string Apellidos { get; internal set; }
+        public string Email { get; internal set; }
+        public string Telefono { get; internal set; }
+    }
+}
--- Formulario de empleado Aitor/MainWindow.xaml.cs
+++ Formulario de empleado Aitor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Formulario_de_empleado_Aitor.Properties;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,8 @@ namespace Formulario_de_empleado_Aitor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<Empleado> empleado = new List<Empleado>();
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +37,28 @@ namespace Formulario_de_empleado_Aitor
         private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+        private void btn_CargarFoto(object sender, TextChangedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.gif;*.bmp|Todos los archivos|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    // Crear un objeto BitmapImage para mostrar la imagen.
+                    BitmapImage bitmapImage = new BitmapImage(new Uri(filePath));
+
+                    // Asignar la imagen al control Image en tu interfaz de usuario.
+                    imageControl.Source = bitmapImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+                }
+            }
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -73,6 +99,38 @@ namespace Formulario_de_empleado_Aitor
                 }
             }
         }
+
+        private void botonGuardar_Click(object sender, RoutedEventArgs e)
+{
+    if (!string.IsNullOrWhiteSpace(Nombre.Text) &&
+        !string.IsNullOrWhiteSpace(Apellido.Text) &&
+        !string.IsNullOrWhiteSpace(Email.Text) &&
+        !string.IsNullOrWhiteSpace(Telefono.Text))
+    {
+        // Crear un nuevo empleado y agregarlo a la lista
+        Empleado nuevoEmpleado = new Empleado
+        {
+            Nombre = Nombre.Text,
+            Apellidos = Apellido.Text,
+            Email = Email.Text,
+            Telefono = Telefono.Text
+        };
+
+        empleado.Add(nuevoEmpleado);
+
+        // Actualizar el DataGrid
+        // Limpiar los campos
+        Nombre.Text = "";
+        Apellido.Text = "";
+        Email.Text = "";
+        Telefono.Text = "";
+    }
+    else
+    {
+        MessageBox.Show("Por favor complete todos los campos.", "Error de validación", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
+
     }
 
 }
