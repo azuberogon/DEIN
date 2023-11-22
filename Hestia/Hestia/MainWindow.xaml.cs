a25820af377b06df86ccc34e0d6c558eace7ef05
ZuberoClase <azuberogondam1b@gmail.com>
2023-11-22 16:36:29 +0100

    definicion de botones

--- Hestia/Hestia/MainWindow.xaml.cs
+++ Hestia/Hestia/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.ventanas;
 
 namespace Hestia
 {
@@ -23,6 +24,17 @@ namespace Hestia
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_Ajustas(object sender, RoutedEventArgs e)
+        {
+            Window musica = new musica();
+            nuevaVentana.Show();
+        }
+
+        private void Button_home(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
