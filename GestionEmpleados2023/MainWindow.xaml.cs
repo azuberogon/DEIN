d53aa05574e49e590388f64c4af1f81f7f6435d8
ZuberoClase <azuberogondam1b@gmail.com>
2023-12-01 16:34:51 +0100

    fallos

--- GestionEmpleados2023/MainWindow.xaml.cs
+++ GestionEmpleados2023/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,52 @@ namespace GestionEmpleados2023
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public class Empleado { 
+            public String Nombre { get; set; }
+            public String Apellido { get; set; }
+            public bool EsUsuario{ get; set; }
+            public int Edad { get; set; }
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public partial class GestionEmpleados2023 {
+            private SqlConnection conexionConSql;
+            public GestionEmpleados2023() {
+                EstablecerConexion();
+            }
+            private void EstablecerConexion() {
+                string CadenaDeConexion = ConfigurationManager.ConnectionStrings["GestionEmpleados2023.Properties.Settings.GestionEmpleadosConnectionString"].ConnectionString;
+                conexionConSql = new SqlConnection(CadenaDeConexion);
+            }
+            public List<Empleado> ObtenerEmpleados()
+            {
+                EstablecerConexion();
+
+                string consulta = "SELECT * FROM EMPLEADOS";
+                DataTable Empleados = new DataTable();
+
+                List<Empleado> listaEmpleados = new List<Empleado>();
+                SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexionConSql);
+
+                using (adaptador)
+                {
+                    adaptador.Fill(Empleados);
+                }
+                listaEmpleados = Empleados.AsEnumerable().Select(row => new Empleado
+                { 
+                    Nombre = row.Field<string>("Nombre"),
+                    Apellido = row.Field<string>("Apellidos"),
+                    EsUsuario = (row["EsUsuario"] != DBNull.Value) ? row.Field<bool>("EsUsuario") : false,
+                    Edad = row.Field<int>("Edad")
+                }).ToList();
+
+                return listaEmpleados;
+            }
         }
     }
 }
