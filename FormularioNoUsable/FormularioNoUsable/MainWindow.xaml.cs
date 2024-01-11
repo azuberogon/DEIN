e275a96d7544ecd90709bcf253d624a638f8e312
aitor zubero <azuberogondam1b@gmail.com>
2024-01-11 01:30:21 +0100

    ultimo no se que es

--- FormularioNoUsable/FormularioNoUsable/MainWindow.xaml.cs
+++ FormularioNoUsable/FormularioNoUsable/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,116 @@ namespace FormularioNoUsable
         {
             InitializeComponent();
         }
+
+        private void CargarImagen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de Imagen|*.jpg;*.jpeg;*.png;*.bmp|Todos los archivos|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string rutaImagen = openFileDialog.FileName;
+
+                // Cargar la imagen seleccionada en el control Image
+                BitmapImage imagenBitmap = new BitmapImage(new Uri(rutaImagen));
+                imagenAmostrar.Source = imagenBitmap;
+            }
+        }
+
+        private void txt_GotFocus(Object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (textBox.Text == "Direccion" || textBox.Text == "Ciudad" || textBox.Text == "Provincia" || textBox.Text == "Codigo Postal" || textBox.Text == "Pais")
+                {
+                    textBox.Text = "";
+                }
+            }
+        }
+
+        private void txt_LostFocus(Object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    if (textBox.Name == "txtDireccion")
+                    {
+                        textBox.Text = "Direccion";
+                    }
+                    else if (textBox.Name == "txtCiudad")
+                    {
+                        textBox.Text = "Ciudad";
+                    }
+                    else if (textBox.Name == "txtProvincia")
+                    {
+                        textBox.Text = "Provincia";
+                    }
+                    else if (textBox.Text == "txtPostal")
+                    {
+                        textBox.Text = "Codigo Postal";
+                    }
+                    else if (textBox.Text == "txtPais")
+                    {
+                        textBox.Text = "Pais";
+                    }
+                }
+            }
+        }
+
+        private void btnGuardar(object sender, RoutedEventArgs e)
+        {
+
+
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Ingresa un nombre valido", "Error");
+            }
+            else if (txtApellidos.Text == "")
+            {
+                MessageBox.Show("Ingresa un apellido valido", "Error");
+            }
+            else if (txtEmail.Text == "")
+            {
+                MessageBox.Show("Ingresa un email valido", "Error");
+            }
+            else if (txtTelefono.Text == "")
+            {
+                MessageBox.Show("Ingresa un telefono valido", "Error");
+            }
+            else
+            {
+                Empleados nuevoEmpleado = new Empleados(txtNombre.Text, txtApellidos.Text, txtEmail.Text, txtTelefono.Text);
+
+
+                datagrid.Items.Add(nuevoEmpleado);
+            }
+
+
+        }
+
+        private void btnCancelar(object sender, RoutedEventArgs e)
+        {
+            MainWindow nuevaVentana = new MainWindow();
+            nuevaVentana.Show();
+            this.Close();
+        }
+
+        public class Empleados
+        {
+            public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public string email { get; set; }
+            public string telefono { get; set; }
+
+            public Empleados(string nombre, string apellidos, string email, string telefono)
+            {
+                this.nombre = nombre;
+                this.apellidos = apellidos;
+                this.email = email;
+                this.telefono = telefono;
+            }
+        }
     }
+
 }
